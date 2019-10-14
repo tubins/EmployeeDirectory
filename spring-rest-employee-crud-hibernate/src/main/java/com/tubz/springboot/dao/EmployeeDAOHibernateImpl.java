8392f23f3817519d086ebde6028299ccb6b95e88
tubins <tubin.shaji@gmail.com>
2019-10-14 15:48:38 +0100

    Setup EntityManager
    Define field for EntityManger and inject it using the constructor
    injection method and @Autowired annotation.

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/dao/EmployeeDAOHibernateImpl.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/dao/EmployeeDAOHibernateImpl.java
@@ -2,12 +2,24 @@ package com.tubz.springboot.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.tubz.springboot.entity.Employee;
 
 @Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	// define field for entity manager
+	private EntityManager entityManger;
+
+	// setup constructor injection
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+		entityManger = theEntityManager;
+	}
 
 	@Override
 	public List<Employee> findAll() {
