66c269ba89dbb421cce5cc28af21e5e4e18d32bc
tubins <tubin.shaji@gmail.com>
2019-10-14 15:29:41 +0100

    Define fields and add mapping

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/entity/Employee.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/entity/Employee.java
@@ -1,9 +1,27 @@
 package com.tubz.springboot.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "employee")
 public class Employee {
+
+	// define fields and add mapping
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
 }
