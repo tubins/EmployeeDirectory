10477b01f538f00d46d2f99a7b6402f9c35f6c66
tubins <tubin.shaji@gmail.com>
2019-10-14 16:14:53 +0100

    Rename main class name

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/SpringRESTEmployeeCRUDHibernateApplication.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/SpringRESTEmployeeCRUDHibernateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestEmployeeCrudHibernateApplication {
+public class SpringRESTEmployeeCRUDHibernateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestEmployeeCrudHibernateApplication.class, args);
+		SpringApplication.run(SpringRESTEmployeeCRUDHibernateApplication.class, args);
 	}
 
 }
