52ff2c2993e4397757f0df27bc582fd8b2dd7bda
tubins <tubin.shaji@gmail.com>
2019-10-14 19:09:04 +0100

    Add mapping for update an Employee

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,13 @@ public class EmployeeController {
 
 		// Just in case user send an id in the JSON, we need to force to add new item
 		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+
+	// Add mapping for update an Employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
 		employeeService.save(theEmployee);
 		return theEmployee;
 	}
