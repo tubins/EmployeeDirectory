25acd37a8fd95ea75886eae004f8cf0b894c11ca
tubins <tubin.shaji@gmail.com>
2019-10-14 14:58:48 +0100

    Add Spring Boot project setup

---
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/SpringRestEmployeeCrudHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.tubz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestEmployeeCrudHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestEmployeeCrudHibernateApplication.class, args);
+	}
+
+}
