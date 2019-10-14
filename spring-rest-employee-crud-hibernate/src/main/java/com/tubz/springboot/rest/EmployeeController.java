d4f8adf7782dd9aff2afed1c1b66e136d8e061dd
tubins <tubin.shaji@gmail.com>
2019-10-14 16:05:40 +0100

    Inject EmployeeDAO

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
@@ -1,12 +1,22 @@
 package com.tubz.springboot.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tubz.springboot.dao.EmployeeDAO;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
 	// Inject EmployeeDAO
+	private EmployeeDAO employeeDAO;
+
+	@Autowired
+	public EmployeeController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+
 	// Expose "/employees" and return list of employees
 }
