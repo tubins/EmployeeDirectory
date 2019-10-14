21461875283ce4507ab6c63458da10743edfbada
tubins <tubin.shaji@gmail.com>
2019-10-14 15:42:27 +0100

    Add EmployeeDAO implementation class

---
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,15 @@
+package com.tubz.springboot.dao;
+
+import java.util.List;
+
+import com.tubz.springboot.entity.Employee;
+
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
