ac0dcd280033576f2331d75afc56652da7a41054
tubins <tubin.shaji@gmail.com>
2019-10-14 18:51:28 +0100

    Add method to POST an Employee

--- spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
+++ spring-rest-employee-crud-hibernate/src/main/java/com/tubz/springboot/rest/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,14 @@ public class EmployeeController {
 		}
 		return theEmployee;
 	}
-	
-	// 
+
+	// add mapping for POST /employees - add a new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+		// Just in case user send an id in the JSON, we need to force to add new item
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
 }
